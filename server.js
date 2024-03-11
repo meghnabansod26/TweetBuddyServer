2d8539e6c31dbac004b1d8001bef9a9ce58ef8ef
meghnabansod26 <meghnabansod26@gmail.com>
2024-03-11 15:08:23 +0530

    changes in package.json

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import { readdirSync } from "fs";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { readdirSync } from 'fs';
+import morgan from 'morgan';
+import { createRequire } from 'module';
 
-const morgan = require("morgan");
-require("dotenv").config();
+const require = createRequire(import.meta.url);
+
+import http from 'http';
+import { Server } from 'socket.io';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-type"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-type'],
   },
 });
 
@@ -24,11 +31,11 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log("DB CONNECTION ERROR => ", err));
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.log('DB CONNECTION ERROR => ', err));
 
 // middlewares
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -37,25 +44,25 @@ app.use(
 );
 
 // autoload routes
-readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`).default));
 
 // socketio
-// io.on("connect", (socket) => {
-//   // console.log("SOCKET>IO", socket.id);
-//   socket.on("send-message", (message) => {
-//     // console.log("new message received => ", message);
-//     socket.broadcast.emit("receive-message", message);
+// io.on('connect', (socket) => {
+//   // console.log('SOCKET>IO', socket.id);
+//   socket.on('send-message', (message) => {
+//     // console.log('new message received => ', message);
+//     socket.broadcast.emit('receive-message', message);
 //   });
 // });
 
-io.on("connect", (socket) => {
-  // console.log("SOCKET>IO", socket.id);
-  socket.on("new-post", (newPost) => {
-    // console.log("socketio new post => ", newPost);
-    socket.broadcast.emit("new-post", newPost);
+io.on('connect', (socket) => {
+  // console.log('SOCKET>IO', socket.id);
+  socket.on('new-post', (newPost) => {
+    // console.log('socketio new post => ', newPost);
+    socket.broadcast.emit('new-post', newPost);
   });
 });
 
 const port = process.env.PORT || 8000;
 
-http.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => console.log(`Server running on port ${port}`));
