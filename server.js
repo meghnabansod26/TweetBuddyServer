558b5015945d97ca25365de0ebd52adedbca9ddf
meghnabansod26 <meghnabansod26@gmail.com>
2024-03-11 15:04:48 +0530

    changes in pkg

--- server.js
+++ server.js
@@ -2,22 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { readdirSync } from "fs";
-import http from "http";
-import * as socketIO from 'socket.io';
-import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
+
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server, {
-  path: "/socket.io",
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-type"],
   },
 });
+
 // db
 mongoose
   .connect(process.env.DATABASE, {
@@ -26,15 +24,11 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log("DB CONNECTION ERROR=>", err));
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR => ", err));
 
-//middlewares
-app.use(
-  express.json({
-    limit: "5mb",
-  })
-);
+// middlewares
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -42,38 +36,26 @@ app.use(
   })
 );
 
-// tweetbuddy.com
-// tweetbuddy.com/api 
-// tweetbuddy.com/socket.io
+// autoload routes
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-//autoload routes
-// readdirSync("./routes").map((r)=>app.use("/api", require(`./routes/${r}`)));
-
-readdirSync("./routes").map(async (r) => {
-  const { default: route } = await import(`./routes/${r}`);
-  app.use("/api", route);
-});
-
-// //socketio
-// io.on("connect", (socket)=>{
-//     // console.log("SOCKET>IO", socket.id);
-
-//     socket.on("send-message", (message)=>{
-//         // console.log("new message received =>", message);
-//         socket.broadcast.emit("receive-message", message);
-//     });
-
+// socketio
+// io.on("connect", (socket) => {
+//   // console.log("SOCKET>IO", socket.id);
+//   socket.on("send-message", (message) => {
+//     // console.log("new message received => ", message);
+//     socket.broadcast.emit("receive-message", message);
+//   });
 // });
 
 io.on("connect", (socket) => {
   // console.log("SOCKET>IO", socket.id);
-
   socket.on("new-post", (newPost) => {
-    // console.log("socketio new post =>", newPost);
+    // console.log("socketio new post => ", newPost);
     socket.broadcast.emit("new-post", newPost);
   });
 });
 
 const port = process.env.PORT || 8000;
 
-server.listen(port, () => console.log(`Server running on port ${port}`));
+http.listen(port, () => console.log(`Server running on port ${port}`));
