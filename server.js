3aa34ea935bf307729f04cec1bd478173fb7940d
meghnabansod26 <meghnabansod26@gmail.com>
2024-03-11 15:10:55 +0530

    changes in package.json

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import { readdirSync } from 'fs';
-import morgan from 'morgan';
-import { createRequire } from 'module';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { readdirSync } from "fs";
+import morgan from "morgan";
+import http from "http";
+import { Server } from "socket.io";
 
-const require = createRequire(import.meta.url);
-
-import http from 'http';
-import { Server } from 'socket.io';
-
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-type'],
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-type"],
   },
 });
 
@@ -31,11 +27,11 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.log('DB CONNECTION ERROR => ', err));
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR => ", err));
 
 // middlewares
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
@@ -44,22 +40,22 @@ app.use(
 );
 
 // autoload routes
-readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`).default));
+readdirSync("./routes").map((r) => app.use("/api", import(`./routes/${r}`)));
 
 // socketio
-// io.on('connect', (socket) => {
-//   // console.log('SOCKET>IO', socket.id);
-//   socket.on('send-message', (message) => {
-//     // console.log('new message received => ', message);
-//     socket.broadcast.emit('receive-message', message);
+// io.on("connect", (socket) => {
+//   // console.log("SOCKET>IO", socket.id);
+//   socket.on("send-message", (message) => {
+//     // console.log("new message received => ", message);
+//     socket.broadcast.emit("receive-message", message);
 //   });
 // });
 
-io.on('connect', (socket) => {
-  // console.log('SOCKET>IO', socket.id);
-  socket.on('new-post', (newPost) => {
-    // console.log('socketio new post => ', newPost);
-    socket.broadcast.emit('new-post', newPost);
+io.on("connect", (socket) => {
+  // console.log("SOCKET>IO", socket.id);
+  socket.on("new-post", (newPost) => {
+    // console.log("socketio new post => ", newPost);
+    socket.broadcast.emit("new-post", newPost);
   });
 });
 
