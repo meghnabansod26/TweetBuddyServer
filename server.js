33f3c0b3a7620121e66afc660f68363dfd034a9a
meghnabansod26 <meghnabansod26@gmail.com>
2024-03-11 15:22:19 +0530

    changed in server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// Importing necessary modules
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import { readdirSync } from 'fs';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import http from 'http';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
-// Configuring environment variables
 dotenv.config();
 
-// Creating Express app and HTTP server
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -23,7 +19,6 @@ const io = new Server(server, {
   },
 });
 
-// Connecting to the database
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -34,7 +29,6 @@ mongoose
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('DB CONNECTION ERROR => ', err));
 
-// Middleware setup
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -43,18 +37,19 @@ app.use(
   })
 );
 
-// Autoload routes
-readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`).default));
+// Autoload routes using dynamic imports
+const routes = readdirSync('./routes');
+for (const route of routes) {
+  const routeModule = await import(`./routes/${route}`);
+  app.use('/api', routeModule.default);
+}
 
-// Socket.io setup
 io.on('connect', (socket) => {
   socket.on('new-post', (newPost) => {
     socket.broadcast.emit('new-post', newPost);
   });
 });
 
-// Setting up port
 const port = process.env.PORT || 8000;
 
-// Starting the server
 server.listen(port, () => console.log(`Server running on port ${port}`));
